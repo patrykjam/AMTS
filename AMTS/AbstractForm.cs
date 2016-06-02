650c3479b4aacd9aa7361f5d8b43770e42a2770c
patrykvan <patrykvan@gmail.com>
2016-06-02 17:57:21 +0200

    Font

--- AMTS/AbstractForm.cs
+++ AMTS/AbstractForm.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing.Text;
+using System.IO;
+using System.Windows.Forms;
 
 namespace AMTS
 {
     public partial class AbstractForm : Form
     {
         protected bool openedWindow = false;
+        PrivateFontCollection PFC;
         public void changeOpenedWindow()
         {
             openedWindow = !openedWindow;
@@ -12,6 +16,12 @@ namespace AMTS
         public AbstractForm()
         {
             InitializeComponent();
+            PFC = new PrivateFontCollection();
+            string fileName = "MyriadPro.ttf";
+            string path = Path.Combine(Environment.CurrentDirectory, @"", fileName);
+            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\Data\" + fileName));
+            PFC.AddFontFile(newPath);
+
         }
     }
 }
