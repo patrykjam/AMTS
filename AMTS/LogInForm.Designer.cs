49f7554a50e5188173d3a7525120036d2adda778
patrykvan <patrykvan@gmail.com>
2016-06-06 22:43:44 +0200

    Poprawka

--- AMTS/LogInForm.Designer.cs
+++ AMTS/LogInForm.Designer.cs
@@ -29,53 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInForm));
-            this.email = new System.Windows.Forms.Label();
-            this.emailTextBox = new System.Windows.Forms.TextBox();
-            this.haslo = new System.Windows.Forms.Label();
-            this.passwordTextBox = new System.Windows.Forms.TextBox();
-            this.incorrectLabel = new System.Windows.Forms.Label();
-            this.logInButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.logInButton = new System.Windows.Forms.Button();
+            this.incorrectLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.haslo = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // email
+            // cancelButton
             // 
-            this.email.AutoSize = true;
-            this.email.BackColor = System.Drawing.Color.Transparent;
-            this.email.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
-            this.email.Location = new System.Drawing.Point(12, 37);
-            this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(87, 18);
-            this.email.TabIndex = 0;
-            this.email.Text = "Adres e-mail";
+            this.cancelButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
+            this.cancelButton.Location = new System.Drawing.Point(15, 141);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 31);
+            this.cancelButton.TabIndex = 6;
+            this.cancelButton.Text = "Anuluj";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
-            // emailTextBox
+            // logInButton
             // 
-            this.emailTextBox.Location = new System.Drawing.Point(96, 37);
-            this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(163, 22);
-            this.emailTextBox.TabIndex = 1;
-            this.emailTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.emailTextBox_KeyDown);
-            // 
-            // haslo
-            // 
-            this.haslo.AutoSize = true;
-            this.haslo.BackColor = System.Drawing.Color.Transparent;
-            this.haslo.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
-            this.haslo.Location = new System.Drawing.Point(13, 65);
-            this.haslo.Name = "haslo";
-            this.haslo.Size = new System.Drawing.Size(42, 18);
-            this.haslo.TabIndex = 2;
-            this.haslo.Text = "Hasło";
-            // 
-            // passwordTextBox
-            // 
-            this.passwordTextBox.Location = new System.Drawing.Point(96, 62);
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(163, 22);
-            this.passwordTextBox.TabIndex = 3;
-            this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordTextBox_KeyDown);
+            this.logInButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logInButton.Location = new System.Drawing.Point(279, 137);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(75, 35);
+            this.logInButton.TabIndex = 5;
+            this.logInButton.Text = "Zaloguj";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
             // 
             // incorrectLabel
             // 
@@ -90,27 +73,44 @@
             this.incorrectLabel.TabIndex = 4;
             this.incorrectLabel.Text = "Sprawdź wprowadzone informacje i spróbuj ponownie.";
             // 
-            // logInButton
+            // passwordTextBox
             // 
-            this.logInButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.logInButton.Location = new System.Drawing.Point(279, 137);
-            this.logInButton.Name = "logInButton";
-            this.logInButton.Size = new System.Drawing.Size(75, 35);
-            this.logInButton.TabIndex = 5;
-            this.logInButton.Text = "Zaloguj";
-            this.logInButton.UseVisualStyleBackColor = true;
-            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            this.passwordTextBox.Location = new System.Drawing.Point(105, 62);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(163, 22);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordTextBox_KeyDown);
             // 
-            // cancelButton
+            // haslo
             // 
-            this.cancelButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
-            this.cancelButton.Location = new System.Drawing.Point(15, 141);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 31);
-            this.cancelButton.TabIndex = 6;
-            this.cancelButton.Text = "Anuluj";
-            this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            this.haslo.AutoSize = true;
+            this.haslo.BackColor = System.Drawing.Color.Transparent;
+            this.haslo.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
+            this.haslo.Location = new System.Drawing.Point(13, 65);
+            this.haslo.Name = "haslo";
+            this.haslo.Size = new System.Drawing.Size(42, 18);
+            this.haslo.TabIndex = 2;
+            this.haslo.Text = "Hasło";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(105, 37);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(163, 22);
+            this.emailTextBox.TabIndex = 1;
+            this.emailTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.emailTextBox_KeyDown);
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.BackColor = System.Drawing.Color.Transparent;
+            this.email.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
+            this.email.Location = new System.Drawing.Point(12, 37);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(87, 18);
+            this.email.TabIndex = 0;
+            this.email.Text = "Adres e-mail";
             // 
             // LogInForm
             // 
